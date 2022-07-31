c66e83b109a1ede38a0399939bb873d646c72d23
AsherRCarney <44686311+AsherrCarney@users.noreply.github.com>
2022-07-31 01:29:32 -0400

    Added noise function that affects the other noise functions

--- MapPro/Assets/Scripts/MapScripts/LandMap.cs
+++ MapPro/Assets/Scripts/MapScripts/LandMap.cs
@@ -8,20 +8,22 @@ public class LandMap
     private PerlinNoise _heightMap = new PerlinNoise(Dynamic.MapData.scale, Constant.Map.range, Constant.Map.seed);
     private static PerlinNoise _heatMap = new PerlinNoise(250, 10, new Vector2(23628, 2479));
     private static PerlinNoise _rainMap = new PerlinNoise(150, 10, new Vector2(98428, 5479));
+    private static PerlinNoise _edgeNoise = new PerlinNoise(12, 2, new Vector2(5000, 79278));
 
     private int GetLayer(float height)
     {
+        
         float water = Dynamic.MapData.waterLevel;
         if (height < water / 2) return 0;
         if (height < water) return 1;
-        if (height < water + 1) return 2;
+        if (height < water + 0.3f) return 2;
         return 3;
     }
 
     private Biome GetBiome(int x, int y)
     {
-        float heat = _heatMap.GetValue(x, y);
-        float rain = _rainMap.GetValue(x, y);
+        float heat = _heatMap.GetValue(x, y) + _edgeNoise.GetValue(x, y) / 4;
+        float rain = _rainMap.GetValue(x, y) + _edgeNoise.GetValue(x, y) / 4;
 
         for (int i = 0; i < GameAssets.instance.biomes.Count; i++)
         {
@@ -40,7 +42,8 @@ public class LandMap
     
     public TileBase GetTile(int x, int y)
     {
-        int layer = GetLayer(_heightMap.GetValue(x, y));
+        float height = _heightMap.GetValue(x, y) + _edgeNoise.GetValue(x, y) / 2;
+        int layer = GetLayer(height);
         Biome biome = GetBiome(x, y);
 
         if (biome.groundTiles[layer] == null)
