fe3fad12e11e2eed1ffde7de344a6da6cc3ac661
AsherRCarney <44686311+AsherrCarney@users.noreply.github.com>
2022-08-01 04:06:18 -0400

    Added more UI control

--- MapPro/Assets/Scripts/UI/UIController.cs
+++ MapPro/Assets/Scripts/UI/UIController.cs
@@ -9,6 +9,7 @@ public class UIController : MonoBehaviour
     public Slider scaleSlider;
     public Slider speedSlider;
     public Slider waterLevel;
+    public Slider sharpnessSlider;
     public Button applyButton;
     public Button exitButton;
 
@@ -20,6 +21,9 @@ public class UIController : MonoBehaviour
         scaleSlider.minValue = 20;
         scaleSlider.maxValue = 150;
         scaleSlider.value = 100;
+        biomeScaleSlider.minValue = 10;
+        biomeScaleSlider.maxValue = 100;
+        biomeScaleSlider.value = 50;
 
         speedSlider.minValue = 10;
         speedSlider.maxValue = 50;
@@ -27,6 +31,10 @@ public class UIController : MonoBehaviour
 
         waterLevel.maxValue = 6;
         waterLevel.value = 2;
+        
+        sharpnessSlider.minValue = 5;
+        sharpnessSlider.maxValue = 15;
+        sharpnessSlider.value = 10;
 
         applyButton.onClick.AddListener(ApplyChanges);
         exitButton.onClick.AddListener(Exit);
@@ -38,7 +46,9 @@ public class UIController : MonoBehaviour
         // Sets functional values to equal ui values
         Dynamic.Player.speed = speedSlider.value;
         Dynamic.MapData.scale = scaleSlider.value;
+        Dynamic.MapData.biomeScale = biomeScaleSlider.value;
         Dynamic.MapData.waterLevel = waterLevel.value;
+        Dynamic.MapData.sharpness = sharpnessSlider.value;
 
         EventManager.instance.UpdateMap();
     }
